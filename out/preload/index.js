95b6de64dafdca86c8e6b7a0a88fbcced6c0236f
kaiyikang <kaiyikang@gmail.com>
2026-01-21 22:02:16 +0100

    chore: delete compiled main and preload JavaScript files.

--- out/preload/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("api", {
-  checkOllamaStatus: () => electron.ipcRenderer.invoke("ollama:check-status")
-});
