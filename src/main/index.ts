cea7b26ea04d419f70cbfc485049c87023dd50d7
ValMobYKang <yikai.kang@valtech.com>
2026-02-03 10:25:09 +0100

    fix: Update console log message and improve CSS styles for container and panel layout

--- src/main/index.ts
+++ src/main/index.ts
@@ -7,7 +7,7 @@ import { readFromClipboard } from '@usecase/readFromClipboard';
 import { getConfig, updateConfig, resetConfig } from '@infrastructure/config';
 import { join } from 'path';
 
-console.log('配置文件目录:', app.getPath('userData'));
+console.log('Configuration File Dir:', app.getPath('userData'));
 
 let mainWindow: BrowserWindow | null = null;
 let settingsWindow: BrowserWindow | null = null;
